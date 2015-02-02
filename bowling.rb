5609ea748d5a28e7430185bd03a12365def65b10
roxm <rossymr@gmail.com>
2015-02-01 22:13:48 -0600

    isStrike and isSpare extracted

--- bowling.rb
+++ bowling.rb
@@ -1,6 +1,7 @@
 class Bowling
   def initialize
 	@rolls =[] 
+	@rollIndex = 0
 	@frames = ['frame1','frame2','frame3','frame4','frame5','frame6','frame7','frame8','frame9','frame10']
   end
   
@@ -10,21 +11,29 @@ class Bowling
 
   def score
 	result = 0
-	rollIndex = 0
+
 	@frames.each do |frame|
-		if @rolls[rollIndex] == 10
-		    result += 10 + @rolls[rollIndex + 1]+ @rolls[rollIndex + 2]
-		    rollIndex+=1
-		elsif @rolls[rollIndex] + @rolls[rollIndex + 1] == 10
-			result += 10 + @rolls[rollIndex + 2]	
-			rollIndex+=2
+		if isStrike
+		    result += 10 + @rolls[@rollIndex + 1]+ @rolls[@rollIndex + 2]
+		    @rollIndex+=1
+		elsif isSpare
+			result += 10 + @rolls[@rollIndex + 2]	
+			@rollIndex+=2
 		else
-			result += @rolls[rollIndex]+@rolls[rollIndex+1]
-			rollIndex+=2
+			result += @rolls[@rollIndex]+@rolls[@rollIndex+1]
+			@rollIndex+=2
 		end
 		
 	end
     return result
+  end
+  
+  def isStrike
+  	return @rolls[@rollIndex] == 10 ? true : false
+  end
+  
+  def isSpare
+  	return  @rolls[@rollIndex] + @rolls[@rollIndex + 1] == 10 ? true : false
   end
   
 end
